6aea66454d66466f157ceed6adce1d4932fd9f7a
effiomduke@gmail.com <effiomduke@gmail.com>
2020-03-27 17:12:23 +0100

    support for json

--- tests/test_app/urls.py
+++ tests/test_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from .views import TestView
+from .views import TestViewMultipart, TestViewJSON
 
 urlpatterns = [
-    re_path(r'^test/$', TestView.as_view(), name='test_view'),
+    re_path(r'^test-multipart/$', TestViewMultipart.as_view(), name='test_multipart'),
+    re_path(r'^test-json/$', TestViewJSON.as_view(), name='test_json')
 ]
--- tests/test_app/views.py
+++ tests/test_app/views.py
@@ -2,10 +2,18 @@ from rest_framework.views import APIView
 from rest_framework.parsers import FormParser
 from rest_framework.response import Response
 
-from ...drf_nested_formdata.parser import NestedMultpartParser
+from ...drf_nested_formdata.parser import NestedMultpartParser, NestedJSONParser
 
-class TestView(APIView):
+class TestViewMultipart(APIView):
     parser_classes = (NestedMultpartParser, FormParser)
+
+    def post(self, request):
+
+        return Response(data=request.data, status=200)
+
+
+class TestViewJSON(APIView):
+    parser_classes = (NestedJSONParser, FormParser)
 
     def post(self, request):
 
