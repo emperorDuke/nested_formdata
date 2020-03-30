5c65de2e7e1ea55028d83c501ad6c670b86cb11a
effiomduke@gmail.com <effiomduke@gmail.com>
2020-03-30 11:17:24 +0100

    typo fix

--- runtests.py
+++ runtests.py
@@ -2,21 +2,13 @@ import os
 import sys
 
 import django
-from django.test.runner import DiscoverRunner
-
-
-class NoDbTestRunner(DiscoverRunner):
-
-    def setup_databases(self, **kwargs):
-        pass
-
-    def teardown_databases(self, config, **kwargs):
-        pass
-
+from django.conf import settings
+from django.test.utils import get_runner
 
 if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
     django.setup()
-    test_runner = NoDbTestRunner()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
     failures = test_runner.run_tests(['tests'])
     sys.exit(bool(failures))
